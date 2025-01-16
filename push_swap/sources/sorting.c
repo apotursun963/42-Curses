34d84e6a39664b44e1cfa2094d7e9ef98c4e2d00
apotursun963 <tursunapo333@gmail.com>
2025-01-16 20:54:44 +0300

    optimizing alog

--- push_swap/sources/sorting.c
+++ push_swap/sources/sorting.c
@@ -44,33 +44,34 @@ void    sort_stack_if_size_3(t_stack **stack_a)
 
 void    sort_under_three_in_a(t_stack **stack_a, t_stack **stack_b, int len)
 {
+    (void)stack_b;
     if (len == 3 && stack_size(*stack_a) == 3)
         sort_stack_if_size_3(stack_a);
     else if (len == 2)
     {
         if ((*stack_a)->data > (*stack_a)->next->data)
             action(stack_a, NULL, SWAP_A);
-    }
-    else if (len == 3)
-    {
-        while (len != 3 || !((*stack_a)->data < (*stack_a)->next->data 
-                && (*stack_a)->next->data < (*stack_a)->next->next->data))
-        {
-            if (len == 3 && (*stack_a)->data > (*stack_a)->next->data 
-                    && (*stack_a)->next->next->data)
-                action(stack_a, NULL, SWAP_A);
-            else if (len == 3 && !((*stack_a)->next->next->data > (*stack_a)->data 
-                    && (*stack_a)->next->next->data > (*stack_a)->next->data))
-            {
-                action(stack_a, stack_b, PUSH_B);
-                len--;
-            }
-            else if ((*stack_a)->data > (*stack_a)->next->data)
-                action(stack_a, NULL, SWAP_A);
-            else if (len++)
-                action(stack_a, stack_b, PUSH_A);
-        }
-    }
+    }   // bu koşulu kaldırabilirsin
+    // else if (len == 3)
+    // {
+    //     while (len != 3 || !((*stack_a)->data < (*stack_a)->next->data 
+    //             && (*stack_a)->next->data < (*stack_a)->next->next->data))
+    //     {
+    //         if (len == 3 && (*stack_a)->data > (*stack_a)->next->data 
+    //                 && (*stack_a)->next->next->data)
+    //             action(stack_a, NULL, SWAP_A);
+    //         else if (len == 3 && !((*stack_a)->next->next->data > (*stack_a)->data 
+    //                 && (*stack_a)->next->next->data > (*stack_a)->next->data))
+    //         {
+    //             action(stack_a, stack_b, PUSH_B);
+    //             len--;
+    //         }
+    //         else if ((*stack_a)->data > (*stack_a)->next->data)
+    //             action(stack_a, NULL, SWAP_A);
+    //         else if (len++)
+    //             action(stack_a, stack_b, PUSH_A);
+    //     }
+    // }
 }
 
 void    sort_under_three_in_b(t_stack **stack_a, t_stack **stack_b, int len)
@@ -103,77 +104,328 @@ void    sort_under_three_in_b(t_stack **stack_a, t_stack **stack_b, int len)
 	}
 }
 
+
+ /*en  gelişmiş */
 int quick_sort_b(t_stack **stack_a, t_stack **stack_b, int len)
 {
     int pivot;
-    int pushed;
-    int i;
+    int total_elements = len;
+    int rotations = 0;
 
     if (is_stack_sorted(*stack_b, DESCENDING))
     {
-        i = 0;
-        while (i++ < len)
+        while (len--)
             action(stack_a, stack_b, PUSH_A);
-        return (0);
+        return (1);
     }
     if (len <= 3)
-        return (sort_under_three_in_b(stack_a, stack_b, len), 0);
-    pushed = 0;
+        return (sort_under_three_in_b(stack_a, stack_b, len), 1);
     pivot = median(stack_b, len);
-    i = 0;
-    while (i < len)
+    while (len > total_elements / 2)
     {
         if ((*stack_b)->data >= pivot)
         {
             action(stack_a, stack_b, PUSH_A);
-            pushed++;
+            len--;
         }
         else
-            action(NULL, stack_b, ROTATE_B);
-        i++;
+        {
+            if (!is_stack_sorted(*stack_b, DESCENDING))
+            {
+                action(NULL, stack_b, ROTATE_B);
+                rotations++;
+            }
+        }
     }
-    i = 0;
-    while (i < len - pushed)
-    {
+    while (total_elements / 2 < stack_size(*stack_b) && rotations--)
         action(NULL, stack_b, REVERSE_ROTATE_B);
-        i++;
-    }
-    quick_sort_a(stack_a, stack_b, pushed);
-    quick_sort_b(stack_a, stack_b, len - pushed);
-    return (0);
+    quick_sort_a(stack_a, stack_b, total_elements / 2 + total_elements % 2);
+    quick_sort_b(stack_a, stack_b, total_elements / 2);
+    return (1);
 }
 
 int quick_sort_a(t_stack **stack_a, t_stack **stack_b, int len)
 {
     int pivot;
-    int pushed;
-    int i;
+    int total_elements = len;
+    int rotations = 0;
 
     if (is_stack_sorted(*stack_a, ASCENDING))
-        return (0);
+        return (1);
     if (len <= 3)
-        return (sort_under_three_in_a(stack_a, stack_b, len), 0);
-    pushed = 0;
+        return (sort_under_three_in_a(stack_a, stack_b, len), 1);
     pivot = median(stack_a, len);
-    i = 0;
-    while (i < len)
+    while (len > total_elements / 2 + total_elements % 2)
     {
         if ((*stack_a)->data < pivot)
         {
             action(stack_a, stack_b, PUSH_B);
-            pushed++;
+            len--;
         }
         else
-            action(stack_a, NULL, ROTATE_A);
-        i++;
+        {
+            if (!is_stack_sorted(*stack_a, ASCENDING))
+            {
+                action(stack_a, NULL, ROTATE_A);
+                rotations++;
+            }
+        }
     }
-    i = 0;
-    while (i < len - pushed)
-    {
+    while (total_elements / 2 + total_elements % 2 < stack_size(*stack_a) && rotations--)
         action(stack_a, NULL, REVERSE_ROTATE_A);
-        i++;
-    }
-    quick_sort_a(stack_a, stack_b, len - pushed);
-    quick_sort_b(stack_a, stack_b, pushed);
-    return (0);
+    quick_sort_a(stack_a, stack_b, total_elements / 2 + total_elements % 2);
+    quick_sort_b(stack_a, stack_b, total_elements / 2);
+    return (1);
 }
+
+
+
+
+// /*daha da gelişmiş */
+// int quick_sort_b(t_stack **stack_a, t_stack **stack_b, int len)
+// {
+//     int pivot;
+//     int total_elements = len;
+//     int rotations = 0;
+
+//     // Eğer stack_b sıralıysa tüm elemanları stack_a'ya taşı
+//     if (is_stack_sorted(*stack_b, DESCENDING))
+//     {
+//         while (len--)
+//             action(stack_a, stack_b, PUSH_A);
+//         return (1);
+//     }
+
+//     // Küçük gruplar için farklı sıralama işlemi
+//     if (len <= 3)
+//     {
+//         sort_under_three_in_b(stack_a, stack_b, len);
+//         return (1);
+//     }
+
+//     // Pivot hesapla
+//     pivot = median(stack_b, len);
+
+//     // Elemanları pivot'a göre ayır
+//     while (len > total_elements / 2)
+//     {
+//         if ((*stack_b)->data >= pivot)
+//         {
+//             action(stack_a, stack_b, PUSH_A);
+//             len--;
+//         }
+//         else
+//         {
+//             action(NULL, stack_b, ROTATE_B);
+//             rotations++;
+//         }
+//     }
+
+//     // Döndürme işlemlerini geri al
+//     while (total_elements / 2 < stack_size(*stack_b) && rotations--)
+//         action(NULL, stack_b, REVERSE_ROTATE_B);
+
+//     // Alt gruplar üzerinde rekürsif çağrılar
+//     quick_sort_a(stack_a, stack_b, total_elements / 2 + total_elements % 2);
+//     quick_sort_b(stack_a, stack_b, total_elements / 2);
+
+//     return (1);
+// }
+
+// int quick_sort_a(t_stack **stack_a, t_stack **stack_b, int len)
+// {
+//     int pivot;
+//     int total_elements = len;
+//     int rotations = 0;
+
+//     // Eğer stack_a sıralıysa işlem yapmadan dön
+//     if (is_stack_sorted(*stack_a, ASCENDING))
+//         return (1);
+
+//     // Küçük gruplar için farklı sıralama işlemi
+//     if (len <= 3)
+//     {
+//         sort_under_three_in_a(stack_a, stack_b, len);
+//         return (1);
+//     }
+
+//     // Pivot hesapla
+//     pivot = median(stack_a, len);
+
+//     // Elemanları pivot'a göre ayır
+//     while (len > total_elements / 2 + total_elements % 2)
+//     {
+//         if ((*stack_a)->data < pivot)
+//         {
+//             action(stack_a, stack_b, PUSH_B);
+//             len--;
+//         }
+//         else
+//         {
+//             action(stack_a, NULL, ROTATE_A);
+//             rotations++;
+//         }
+//     }
+
+//     // Döndürme işlemlerini geri al
+//     while (total_elements / 2 + total_elements % 2 < stack_size(*stack_a) && rotations--)
+//         action(stack_a, NULL, REVERSE_ROTATE_A);
+
+//     // Alt gruplar üzerinde rekürsif çağrılar
+//     quick_sort_a(stack_a, stack_b, total_elements / 2 + total_elements % 2);
+//     quick_sort_b(stack_a, stack_b, total_elements / 2);
+
+//     return (1);
+// }
+
+
+
+
+// /*orta gelişmiş*/
+// int quick_sort_b(t_stack **stack_a, t_stack **stack_b, int len)
+// {
+//     int pivot;
+//     int numbers;
+//     int rotated = 0;
+
+//     if (is_stack_sorted(*stack_b, DESCENDING))
+//     {
+//         while (len--)
+//             action(stack_a, stack_b, PUSH_A);
+//         return (1);
+//     }
+//     numbers = len;
+//     if (len <= 3)
+//         return (sort_under_three_in_b(stack_a, stack_b, len), 1);
+//     pivot = median(stack_b, len);
+//     while (len > numbers / 2)
+//     {
+//         if ((*stack_b)->data >= pivot)
+//         {
+//             action(stack_a, stack_b, PUSH_A);
+//             len--;
+//         }
+//         else
+//         {
+//             action(NULL, stack_b, ROTATE_B);
+//             rotated++;
+//         }
+//     }
+//     while (numbers / 2 < stack_size(*stack_b) && rotated--)
+//         action(NULL, stack_b, REVERSE_ROTATE_B);
+//     quick_sort_a(stack_a, stack_b, numbers / 2 + numbers % 2);
+//     quick_sort_b(stack_a, stack_b, numbers / 2);
+//     return (1);
+// }
+
+// int quick_sort_a(t_stack **stack_a, t_stack **stack_b, int len)
+// {
+//     int pivot;
+//     int numbers;
+//     int rotated = 0;
+
+//     if (is_stack_sorted(*stack_a, ASCENDING))
+//         return (1);
+//     numbers = len;
+//     if (len <= 3)
+//         return (sort_under_three_in_a(stack_a, stack_b, len), 1);
+//     pivot = median(stack_a, len);
+//     while (len > numbers / 2 + numbers % 2)
+//     {
+//         if ((*stack_a)->data < pivot)
+//         {
+//             action(stack_a, stack_b, PUSH_B);
+//             len--;
+//         }
+//         else
+//         {
+//             action(stack_a, NULL, ROTATE_A);
+//             rotated++;
+//         }
+//     }
+//     while (numbers / 2 + numbers % 2 < stack_size(*stack_a) && rotated--)
+//         action(stack_a, NULL, REVERSE_ROTATE_A);
+//     quick_sort_a(stack_a, stack_b, numbers / 2 + numbers % 2);
+//     quick_sort_b(stack_a, stack_b, numbers / 2);
+//     return (1);
+// }
+
+
+
+
+/* biraz gelişmiş  */
+//  
+// int quick_sort_b(t_stack **stack_a, t_stack **stack_b, int len)
+// {
+//     int pivot;
+//     int pushed;
+//     int i;
+
+//     if (is_stack_sorted(*stack_b, DESCENDING))
+//     {
+//         i = 0;
+//         while (i++ < len)
+//             action(stack_a, stack_b, PUSH_A);
+//         return (0);
+//     }
+//     if (len <= 3)
+//         return (sort_under_three_in_b(stack_a, stack_b, len), 0);
+//     pushed = 0;
+//     pivot = median(stack_b, len);
+//     i = 0;
+//     while (i < len)
+//     {
+//         if ((*stack_b)->data >= pivot)
+//         {
+//             action(stack_a, stack_b, PUSH_A);
+//             pushed++;
+//         }
+//         else
+//             action(NULL, stack_b, ROTATE_B);
+//         i++;
+//     }
+//     i = 0;
+//     while (i < len - pushed)
+//     {
+//         action(NULL, stack_b, REVERSE_ROTATE_B);
+//         i++;
+//     }
+//     quick_sort_a(stack_a, stack_b, pushed);
+//     quick_sort_b(stack_a, stack_b, len - pushed);
+//     return (0);
+// }
+
+// int quick_sort_a(t_stack **stack_a, t_stack **stack_b, int len)
+// {
+//     int pivot;
+//     int pushed;
+//     int i;
+
+//     if (is_stack_sorted(*stack_a, ASCENDING))
+//         return (0);
+//     if (len <= 3)
+//         return (sort_under_three_in_a(stack_a, stack_b, len), 0);
+//     pushed = 0;
+//     pivot = median(stack_a, len);
+//     i = 0;
+//     while (i < len)
+//     {
+//         if ((*stack_a)->data < pivot)
+//         {
+//             action(stack_a, stack_b, PUSH_B);
+//             pushed++;
+//         }
+//         else
+//             action(stack_a, NULL, ROTATE_A);
+//         i++;
+//     }
+//     i = 0;
+//     while (i < len - pushed)
+//     {
+//         action(stack_a, NULL, REVERSE_ROTATE_A);
+//         i++;
+//     }
+//     quick_sort_a(stack_a, stack_b, len - pushed);
+//     quick_sort_b(stack_a, stack_b, pushed);
+//     return (0);
+// }
