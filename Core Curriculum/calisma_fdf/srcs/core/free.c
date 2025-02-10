2ecd8923b300f127e2a50834694a05cc455673ab
Abdullah Tursun <atursun@k1m41s04.42istanbul.com.tr>
2025-02-10 17:58:17 +0300

    sdfsdf

---
+++ Core Curriculum/calisma_fdf/srcs/core/free.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atursun <atursun@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 13:01:08 by atursun           #+#    #+#             */
+/*   Updated: 2025/02/10 12:54:11 by atursun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/fdf.h"
+
+void	free_coordinates(t_point **coordinates, int width)
+{
+	int		i;
+
+	i = 0;
+	while (i < width)
+		free(coordinates[i++]);
+	free(coordinates);
+}
+
+void	free_map(t_fdf *fdf)
+{
+	free_coordinates(fdf->map->coordinates, fdf->map->max_x);
+	free(fdf->map);
+	mlx_destroy_window(fdf->mlx, fdf->win);
+	mlx_destroy_display(fdf->mlx);
+	free(fdf);
+	exit(1);
+}
+
+int	free_all(t_fdf *fdf)
+{
+	free_coordinates(fdf->map->coordinates, fdf->map->max_x);
+	free(fdf->map);
+	mlx_destroy_image(fdf->mlx, fdf->image->image);
+	free(fdf->image);
+	free(fdf->cam);
+	mlx_destroy_window(fdf->mlx, fdf->win);
+	mlx_destroy_display(fdf->mlx);
+	free(fdf->mlx);
+	free(fdf);
+	exit(1);
+	return (0);
+}
---
+++ Core Curriculum/calisma_fdf/srcs/graphics/projection.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   projection.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atursun <atursun@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 12:59:17 by atursun           #+#    #+#             */
+/*   Updated: 2025/02/10 13:38:37 by atursun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/fdf.h"
+
+char	*get_projection_name(t_fdf *fdf)
+{	
+	char	*projection;
+
+	projection = "";
+	if (fdf->cam->projection == ISOMETRIC)
+		projection = "Isometric projection";
+	else if (fdf->cam->projection == PERSPECTIVE)
+		projection = "Perspective projection";
+	else if (fdf->cam->projection == TOP)
+		projection = "Top view";
+	return (projection);
+}
+
+void	write_menu_bar(t_fdf *fdf)
+{
+	int		y;
+	char	*projection;
+	void	*mlx;
+	void	*win;
+
+	y = 0;
+	mlx = fdf->mlx;
+	win = fdf->win;
+	projection = get_projection_name(fdf);
+	mlx_string_put(mlx, win, 50, y += 50, C_TEXT, projection);
+	mlx_string_put(mlx, win, 50, y += 35, C_TEXT, "Press 'ESC' to close");
+	mlx_string_put(mlx, win, 50, y += 35, C_TEXT, "Zoom: press '-' or '+'");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Move: press arrow keys");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Rotate X: press 'S' or 'W'");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Rotate Y: press 'Q' or 'E'");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Rotate Z: press 'A' or 'D'");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Scale Z: press 'Z' or 'X'");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "(Z scale limited to 100%)");
+	mlx_string_put(mlx, win, 50, y += 35, C_TEXT, "To change projection view:");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Isometric: press 'I'");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Perspective: press 'P'");
+	mlx_string_put(mlx, win, 50, y += 20, C_TEXT, "Top View: press 'O'");
+	mlx_string_put(mlx, win, 50, y += 35, C_TEXT, "Colors: press 'SPACE'");
+	mlx_string_put(mlx, win, 50, y += 35, C_TEXT, "Reset view: press 'R'");
+}
+/*
+İzometrik projeksiyon 3D nesneleri 2D düzlemde gösterirken, eksenler arasındaki açıyı 30 derece tutarak, 
+tüm eksenlerde eşit ölçüler sağlar. Yani, x, y ve z eksenleri birbirinden eşit mesafeyle ve eşit derecede uzanır.
+*/
+void	isometric(t_line *line)
+{
+	t_point	new_start;
+	t_point	new_end;
+
+	new_start.x = (line->start.x - line->start.y) * cos(ANG_30);
+	new_start.y = (line->start.x + line->start.y) * sin(ANG_30) - line->start.z;
+	line->start.x = new_start.x;
+	line->start.y = new_start.y;
+	new_end.x = (line->end.x - line->end.y) * cos(ANG_30);
+	new_end.y = (line->end.x + line->end.y) * sin(ANG_30) - line->end.z;
+	line->end.x = new_end.x;
+	line->end.y = new_end.y;
+}
+
+/*
+Perspektif projeksiyonu, gerçek dünyada olduğu gibi, uzaktaki nesnelerin daha küçük, 
+yakındaki nesnelerin ise daha büyük görünmesini sağlar.
+Bu tür projeksiyonlar, genellikle bir gözlemcinin bakış açısından, derinliği yansıtarak görüntü oluşturur.
+Bu fonksiyon, bir çizginin perspektif projeksiyonunu yapar. Perspektif projeksiyon, 
+3D uzaydaki nesneleri 2D düzleme aktarırken, derinliği (z eksenini) dikkate alır ve uzak nesnelerin daha küçük görünmesini sağlar.
+*/
+void	perspective(t_line *line)
+{
+	t_point	new_start;
+	t_point	new_end;
+	double	z;
+
+	// X Ekseninde Döndürme
+	rotate_x(line, 3 * -ANG_45);	// ters döndürüyor çünkü negatif açı
+
+	//  Z Koordinatına Perspektif Uygulama
+
+	/// z nin başlangıç noktası için prespektif
+	z = line->start.z + line->transform_z;
+	new_start.x = line->start.x / z;	
+	new_start.y = line->start.y / z;
+	line->start.x = new_start.x;
+	line->start.y = -new_start.y;
+
+	/// z nin bitiş noktası için prespektif
+	z = line->end.z + line->transform_z;
+	new_end.x = line->end.x / z;
+	new_end.y = line->end.y / z;
+	line->end.x = new_end.x;
+	line->end.y = -new_end.y;
+	scale(line, line->transform_z);
+}
+
+void	projection(t_cam *cam, t_line *line)
+{
+	if (cam->projection == ISOMETRIC)
+		isometric(line);
+	else if (cam->projection == PERSPECTIVE)
+		perspective(line);
+	else if (cam->projection == TOP)
+		return ;
+}
