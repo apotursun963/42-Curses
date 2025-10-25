65a9ff51eddf25a01ed5472b93dd95f9a94c17a2
apotursun963 <atursun@student.42istanbul.com.tr>
2025-10-25 14:25:17 +0300

    ins

--- CPP/CPP.md
+++ CPP/CPP.md
@@ -1,7 +1,3 @@
 
 // Burada CPP00 - CPP09'a kadar öğrendin konuları hepsini (9 konut subjecte yazılı)
 markdown dosyasına yaz
-
-
-
-
--- CPP/CPP08/ex01/main.cpp
+++ CPP/CPP08/ex01/main.cpp
@@ -3,36 +3,52 @@
 
 int main(int, char **)
 {
-    Span sp(5);
+    {
+        Span sp(5);
+    
+        try {
+            sp.addNumber(74);
+            sp.addNumber(4);
+            sp.addNumber(50);
+            sp.addNumber(2);
+        }
+        catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    
+        try {
+            int shn = sp.shortestSpan();
+            std::cout << "Shortest span value is: " << shn << std::endl;
+        }
+        catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    
+        try {
+            int lng = sp.longestSpan();
+            std::cout << "Longest span value is: " << lng << std::endl;
+        }
+        catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
 
-    try {
-        sp.addNumber(74);
-        sp.addNumber(4);
-        sp.addNumber(50);
-        sp.addNumber(2);
-    }
-    catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+    {
+        std::cout << "<<<Testing with 10.000 numbers>>>" << std::endl;
 
-    try {
-        int shn = sp.shortestSpan();
-        std::cout << "Shortest span value is: " << shn << std::endl;
-    }
-    catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    try {
-        int lng = sp.longestSpan();
-        std::cout << "Longest span value is: " << lng << std::endl;
-    }
-    catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
+        Span sp2(10000);
+        try {
+            for (int i=0; i < 10000; i++)
+                sp2.addNumber(std::rand());
+            std::cout << "Longest span for 10.000 numbers: " << sp2.longestSpan() << std::endl;
+            std::cout << "Shortest span for 10.000 numbers: " << sp2.shortestSpan() << std::endl;
+        }
+        catch (std::exception &e) {
+            std::cout << "Exception: " << e.what() << std::endl;
+        }
     }
     return 0;
 }
-
 
 /* ex01
 Span Sınıfı maxmimum N tane sayı saklayabilmesi gerekiyor.
